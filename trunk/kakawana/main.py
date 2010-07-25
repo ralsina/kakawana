a68a41ceff1d8a6e53d161f237b1008f6dcfc23f
roberto.alsina <roberto.alsina@a5501ddc-1fab-11df-a4a7-ed6ba186e60d>
2010-07-25 18:50:57 +0000

    improved html -> text

--- trunk/kakawana/main.py
+++ trunk/kakawana/main.py
@@ -26,9 +26,26 @@ import libgreader as gr
 from reader_client import GoogleReaderClient
 import json
 
+from htmlentitydefs import name2codepoint as n2cp
 def h2t(value):
     "Return the given HTML with all tags stripped."
-    return re.sub(r'<[^>]*?>', '', value)
+    return decode_htmlentities(re.sub(r'<[^>]*?>', '', value))
+
+def substitute_entity(match):
+  ent = match.group(2)
+  if match.group(1) == "#":
+    return unichr(int(ent))
+  else:
+    cp = n2cp.get(ent)
+    if cp:
+      return unichr(cp)
+    else:
+      return match.group()
+
+def decode_htmlentities(string):
+  entity_re = re.compile("&(#?)(\d{1,5}|\w{1,8});")
+  return entity_re.subn(substitute_entity, string)[0]
+
 
 VERSION="0.0.1"
 
