37bc461b2022271a6be71096a46444d3ebc4ead3
roberto.alsina <roberto.alsina@a5501ddc-1fab-11df-a4a7-ed6ba186e60d>
2010-07-30 13:08:18 +0000

    Added star icons

--- trunk/kakawana/icons_rc.py
+++ trunk/kakawana/icons_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Tue Jul 20 16:37:22 2010
+# Created: Fri Jul 30 10:10:40 2010
 #      by: The Resource Compiler for PyQt (Qt v4.6.3)
 #
 # WARNING! All changes made in this file will be lost!
