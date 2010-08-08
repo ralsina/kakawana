b1afaf5c097721431066341bd39d20176e9f307c
roberto.alsina <roberto.alsina@a5501ddc-1fab-11df-a4a7-ed6ba186e60d>
2010-08-08 18:01:57 +0000

    Modern template generated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,38 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import sys, os
 
-from distutils.core import setup
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-setup(name='Kakawana',
-      version='0.1',
-      description='An Internet comic reader',
-      author='Roberto Alsina',
-      author_email='ralsina@netmanagers.com.ar',
-      url='http://kakawana.googlecode.com',
-      packages=['kakawana'],
-      scripts=['kakawana.bin'],
-      package_data={'kakawana':[]},
-     )
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='kakawana',
+    version=version,
+    description="A modern news aggregator",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='rss atom qt pyqt aggregator news',
+    author='Roberto Alsina',
+    author_email='ralsina@netmanagers.com.ar',
+    url='http://kakawana.googlecode.com',
+    license='GPLv2',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['kakawana=kakawana:main']
+    }
+)
